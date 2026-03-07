eedb64a0aeffed9d53695dff92339af35a8258fb
コモディ <nobu54459@gmail.com>
2026-03-07 13:05:58 +0900

    feature/migration

--- src/database/migrations/2014_10_12_000000_create_users_table.php
+++ src/database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,8 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->timestamp('first_login_email_verified_at')->nullable();
+            $table->unsignedBigInteger('status_id')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
